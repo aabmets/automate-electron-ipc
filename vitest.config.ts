d3731b34ae0cd1f71680c52dd3131ff299743999
Mattias Aabmets <mattias.aabmets@gmail.com>
2024-11-01 10:23:34 +0200

    Added vitest.config.ts to externalize typescript dependency

---
+++ vitest.config.ts
@@ -0,0 +1,22 @@
+/*
+ *   MIT License
+ *
+ *   Copyright (c) 2024, Mattias Aabmets
+ *
+ *   The contents of this file are subject to the terms and conditions defined in the License.
+ *   You may not use, modify, or distribute this file except in compliance with the License.
+ *
+ *   SPDX-License-Identifier: MIT
+ */
+
+import { defineConfig } from "vitest/config";
+
+export default defineConfig({
+   test: {
+      server: {
+         deps: {
+            external: ["typescript"],
+         },
+      },
+   },
+});
