e5c6ec12de2c5b7b741397730f1e7a3ad33531ff
Mattias Aabmets <mattias.aabmets@gmail.com>
2024-11-04 18:15:51 +0200

    Added Vite plugin into index.ts

--- src/index.ts
+++ src/index.ts
@@ -8,3 +8,61 @@
  *
  *   SPDX-License-Identifier: MIT
  */
+
+import fsp from "node:fs/promises";
+import path from "node:path";
+import ipc from "@ipc";
+import parser from "@parser";
+import type { IPCAssuredConfig, IPCAutomationOption, IPCOptionalConfig } from "@types";
+import type { CollectedSpecs } from "@types";
+import validators from "@validators";
+import type { Plugin } from "vite";
+
+async function optionProcessor(
+   option: IPCAutomationOption,
+   config: IPCAssuredConfig,
+): Promise<void> {
+   const files = await fsp.readdir(option.mainHandlersDir, { recursive: true });
+   const collection: CollectedSpecs[] = [];
+   for (const file of files) {
+      if (file.startsWith("index.ts")) {
+         continue;
+      }
+      const fullPath = path.join(option.mainHandlersDir, file);
+      const stat = await fsp.stat(fullPath).catch(() => null);
+      if (stat?.isFile()) {
+         const contents = (await fsp.readFile(fullPath)).toString();
+         const specs = parser.parseSpecs(contents);
+         collection.push({
+            fullPath,
+            relativePath: file,
+            parsedSpecs: specs,
+         });
+      }
+   }
+   await ipc.writeMainBindings(option, config, collection);
+}
+
+export function ipcAutomation(
+   options: IPCAutomationOption[],
+   config: IPCOptionalConfig = {},
+): Plugin {
+   const assuredConfig: IPCAssuredConfig = {
+      codeIndent: 4,
+      channelIdentifierLength: 16,
+      namespaceLength: 8,
+      ...config,
+   };
+   validators.validateOptions(options);
+   validators.validateConfig(assuredConfig);
+   return {
+      name: "automate-electron-ipc",
+      buildStart: async () => {
+         const promises: Promise<void>[] = [];
+         options.forEach((option) => {
+            promises.push(optionProcessor(option, assuredConfig));
+         });
+         await Promise.all(promises);
+      },
+   };
+}
