c9521a3b3787bdbadac1f1100dfb0745dda01822
Mattias Aabmets <mattias.aabmets@gmail.com>
2024-11-21 13:04:36 +0200

    Updated ipcAutomation to behavior to match the new configuration structure

--- src/index.ts
+++ src/index.ts
@@ -11,61 +11,82 @@
 
 import fsp from "node:fs/promises";
 import path from "node:path";
-import ipc from "@src/ipc";
-import parser from "@src/parser";
-import validators from "@src/validators";
 import * as t from "@types";
 import type { Plugin } from "vite";
+import parser from "./parser.js";
+import utils from "./utils.js";
+import vld from "./validators.js";
 
-async function optionProcessor(
-   option: t.IPCAutomationOption,
-   config: t.IPCAssuredConfig,
-): Promise<void> {
-   const files = await fsp.readdir(option.mainHandlersDir, { recursive: true });
-   const collection: t.CollectedSpecs[] = [];
-   for (const file of files) {
-      if (file.startsWith("index.ts")) {
-         continue;
-      }
-      const fullPath = path.join(option.mainHandlersDir, file);
-      const stat = await fsp.stat(fullPath).catch(() => null);
-      if (stat?.isFile()) {
-         const contents = (await fsp.readFile(fullPath)).toString();
-         const specs = parser.parseSpecs(contents);
-         collection.push({
-            fullPath,
-            relativePath: file,
-            parsedSpecs: specs,
-         });
-      }
-   }
-   await Promise.all([
-      ipc.writeMainBindings(option, config, collection),
-      ipc.writePreloadBindings(option, config, collection),
-      ipc.writeRendererTypes(option, config, collection),
-   ]);
-}
-
-export function ipcAutomation(
-   options: t.IPCAutomationOption[],
-   config: t.IPCOptionalConfig = {},
-): Plugin {
-   const assuredConfig: t.IPCAssuredConfig = {
-      codeIndent: 4,
-      channelIdentifierLength: 16,
-      namespaceLength: 8,
-      ...config,
-   };
-   validators.validateOptions(options);
-   validators.validateConfig(assuredConfig);
+export function ipcAutomation(config?: t.IPCOptionalConfig): Plugin {
    return {
-      name: "automate-electron-ipc",
+      name: "vite-plugin-automate-electron-ipc",
       buildStart: async () => {
-         const promises: Promise<void>[] = [];
-         options.forEach((option) => {
-            promises.push(optionProcessor(option, assuredConfig));
-         });
-         await Promise.all(promises);
+         const finalConfig = vld.validateResolveConfig(config);
+         const stat = await fsp.stat(finalConfig.ipcSpecPath).catch(() => null);
+         const collectedSpecs: t.CollectedSpecs[] = [];
+
+         if (!stat) {
+            console.warn(
+               "\n ⚠️ - Skipping IPC automation, because the path pointed to by",
+               `ipcSpecPath does not exist:\n      '${finalConfig.ipcSpecPath}'\n`,
+            );
+            return;
+         } else if (stat.isFile()) {
+            const contents = await fsp.readFile(finalConfig.ipcSpecPath);
+            collectedSpecs.push({
+               fullPath: finalConfig.ipcSpecPath,
+               relativePath: config?.ipcSpecPath || "src/ipc-spec.ts",
+               parsedSpecs: parser.parseSpecs(contents.toString()),
+            });
+         } else if (stat.isDirectory()) {
+            const files = await fsp.readdir(finalConfig.ipcSpecPath, { recursive: true });
+            const collectedContents: t.CollectedContents[] = [];
+            await Promise.all(
+               files.map(async (file) => {
+                  const fullPath = path.join(finalConfig.ipcSpecPath, file);
+                  const stat = await fsp.stat(fullPath);
+                  if (stat.isFile() && (await fsp.exists(fullPath))) {
+                     const contents = await fsp.readFile(fullPath);
+                     collectedContents.push({
+                        fullPath,
+                        relativePath: file,
+                        contents: contents.toString(),
+                     });
+                  }
+               }),
+            );
+            for (const item of collectedContents) {
+               collectedSpecs.push({
+                  fullPath: item.fullPath,
+                  relativePath: item.relativePath,
+                  parsedSpecs: parser.parseSpecs(item.contents),
+               });
+            }
+         }
+         for (const item of collectedSpecs) {
+            console.debug(item);
+         }
+         const results: boolean[] = await Promise.all([
+            (async () => true)(), // writeMainBindings(config, collectedFileSpecs),
+            (async () => true)(), // writePreloadBindings(config, collectedFileSpecs),
+            (async () => true)(), // writeRendererTypes(config, collectedFileSpecs),
+         ]);
+         if (results.includes(false)) {
+            console.warn(
+               "\n ⚠️ - Skipping IPC automation, because no Channel definitions",
+               `were found at ipcSpecPath:\n      '${finalConfig.ipcSpecPath}'\n`,
+            );
+         }
       },
    };
 }
+
+export const ipcPreload = {
+   filePath: utils.searchUpwards("user-data/preload.js"),
+};
+
+export function Channel(): void {
+   throw new Error("IPC automation Channel functions cannot be executed by JavaScript.");
+}
+
+export const type = null;
