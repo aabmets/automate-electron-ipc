2a417b2c190993c821caf880bda9653d7e0596ac
Mattias Aabmets <mattias.aabmets@gmail.com>
2024-11-22 01:22:44 +0200

    Parser now correctly parses updated spec definitions from source code

--- src/parser.ts
+++ src/parser.ts
@@ -9,29 +9,9 @@
  *   SPDX-License-Identifier: MIT
  */
 
-import utils from "@src/utils";
 import * as t from "@types";
 import ts from "typescript";
-
-export function getParserRegex(): RegExp {
-   const functionsRegex = utils.concatRegex([
-      /^export\s+(async\s+)?(?<function>function)/,
-      /\s+(?<functionName>\w+)\s*\([^)]*\)\s*(?::\s*[^\n]+)?\s+{\n/,
-   ]);
-   const interfaceRegex = utils.concatRegex([
-      /|^(?<exportedInterface>export\s+)?(?<interface>interface)/,
-      /\s+(?<interfaceName>\w+)\s*{[\s\S]*?\n}\n/,
-   ]);
-   const typeRegex = utils.concatRegex([
-      /|^(?<exportedType>export\s+)?(?<type>type)/,
-      /\s+(?<typeName>\w+)\s*=\s*[\s\S]*?;\n/,
-   ]);
-   const importRegex = utils.concatRegex([
-      /|^(?<import>import\s+)(\*\s+as\s+(?<namespace>\w+)\s+)?/,
-      /[\s\S]*?from\s*['"](?<importPath>.*?)['"];?\n/,
-   ]);
-   return utils.concatRegex([functionsRegex, interfaceRegex, typeRegex, importRegex], "gm");
-}
+import utils from "./utils.js";
 
 export function isBuiltinType(typeName: string): boolean {
    return new Set([
@@ -50,132 +30,192 @@ export function isBuiltinType(typeName: string): boolean {
    ]).has(typeName);
 }
 
-export function collectCustomTypes(
-   node: ts.Node | ts.TypeNode | ts.ImportDeclaration | undefined,
-   customTypes: Set<string>,
-   sourceFile: ts.SourceFile,
-): void {
+export function collectCustomTypes(node: ts.Node, src: ts.SourceFile, set: Set<string>): void {
    if (!node) {
       return;
    } else if (ts.isTypeReferenceNode(node)) {
-      const name = node.typeName.getText(sourceFile);
+      const name = node.typeName.getText(src);
       if (!isBuiltinType(name)) {
-         customTypes.add(name);
+         set.add(name);
       }
    } else if (ts.isTypeLiteralNode(node)) {
       node.members.forEach((member) => {
          if (ts.isPropertySignature(member) && member.type) {
-            collectCustomTypes(member.type, customTypes, sourceFile);
+            collectCustomTypes(member.type, src, set);
          }
       });
    } else if (ts.isUnionTypeNode(node) || ts.isIntersectionTypeNode(node)) {
-      node.types.forEach((subType) => collectCustomTypes(subType, customTypes, sourceFile));
+      node.types.forEach((subType) => collectCustomTypes(subType, src, set));
    } else if (ts.isBindingElement(node)) {
       const children = node.getChildren();
       if (children.length === 3 && ts.isIdentifier(children[2])) {
-         const name = children[2].getText(sourceFile);
+         const name = children[2].getText(src);
          if (!isBuiltinType(name)) {
-            customTypes.add(name);
+            set.add(name);
          }
       }
    } else if (ts.isImportDeclaration(node)) {
       const clause = node.importClause;
       if (clause?.namedBindings && ts.isNamedImports(clause.namedBindings)) {
          clause.namedBindings.elements.forEach((element) => {
-            const name = element.name.getText(sourceFile);
+            const name = element.name.getText(src);
             if ((clause?.isTypeOnly || element.isTypeOnly) && !isBuiltinType(name)) {
-               customTypes.add(name);
+               set.add(name);
             }
          });
       }
    }
-   node.forEachChild((child) => collectCustomTypes(child, customTypes, sourceFile));
+   node.forEachChild((child) => collectCustomTypes(child, src, set));
 }
 
-export function cctFromCode(code: string): Set<string> {
-   const sourceFile = ts.createSourceFile(
-      "temp.ts",
-      utils.dedent(code),
-      ts.ScriptTarget.Latest,
-      true,
-   );
-   const customTypes = new Set<string>();
+const channelPattern = utils.concatRegex([
+   /^Channel\(['"](?<name>\w+)['"]\).(?<kind>Broadcast|Unicast+)/,
+   /.(?<direction>RendererToMain|MainToRenderer|RendererToRenderer)$/,
+]);
 
-   ts.forEachChild(sourceFile, (node: ts.Node) => {
-      collectCustomTypes(node, customTypes, sourceFile);
-   });
-   return customTypes;
+function isSignatureAssignment(text: string): boolean {
+   const regex = /^signature\s*:\s*type +as\s*\(/;
+   return regex.test(text);
 }
 
-export function getFuncSpecs(code: string, skipDedent = false): t.FuncSpec[] {
-   const normalizedCode = skipDedent ? code : utils.dedent(code);
-   const sourceFile = ts.createSourceFile("temp.ts", normalizedCode, ts.ScriptTarget.Latest, true);
-   const funcSpecArray: t.FuncSpec[] = [];
+function isListenersAssignment(text: string): boolean {
+   const regex = /^listeners\s*:\s*\[\s*['"\w\s,]*]$/;
+   return regex.test(text);
+}
 
-   ts.forEachChild(sourceFile, (node: ts.Node) => {
-      if (ts.isFunctionDeclaration(node)) {
-         const customTypes = new Set<string>();
-         collectCustomTypes(node, customTypes, sourceFile);
-         funcSpecArray.push({
-            name: node.name?.text || "",
-            async: (node.modifiers || []).some(
-               (modifier) => modifier.kind === ts.SyntaxKind.AsyncKeyword,
-            ),
-            customTypes: Array.from(customTypes),
-            returnType: node.type ? node.type.getText(sourceFile) : "void",
-            params: (node.parameters || []).map((param) => {
-               return {
-                  name: param.name.getText(sourceFile),
-                  type: param.type ? param.type.getText(sourceFile) : null,
-                  defaultValue: param.initializer ? param.initializer.getText(sourceFile) : null,
-               };
-            }),
+export function parseChannelExpressions(
+   node: ts.Node,
+   src: ts.SourceFile,
+   spec: Partial<t.ChannelSpec>,
+): void {
+   if (ts.isPropertyAccessExpression(node)) {
+      const text = node.getText(src).replaceAll(/\s*/gm, "");
+      const groups = text.match(channelPattern)?.groups;
+      if (groups) {
+         Object.assign(spec, {
+            ...groups,
+            direction: new Map([
+               ["RendererToMain", "R2M"],
+               ["MainToRenderer", "M2R"],
+               ["RendererToRenderer", "R2R"],
+            ]).get(groups.direction),
          });
       }
+   } else if (ts.isPropertyAssignment(node)) {
+      const text = node.getText(src);
+
+      if (isSignatureAssignment(text)) {
+         const sig: Partial<t.CallableSignature> = {};
+         const child1 = node.getChildAt(2);
+         const child2 = child1.getChildAt(2);
+
+         if (ts.isFunctionTypeNode(child2)) {
+            const set = new Set<string>();
+            collectCustomTypes(child2, src, set);
+
+            sig.params = child2.parameters.map((param) => {
+               return {
+                  name: param.name.getText(),
+                  type: param?.type ? param.type.getText() : "any",
+               } as t.CallableParam;
+            });
+            sig.customTypes = Array.from(set);
+            sig.returnType = child2.type.getText(src) || "void";
+            sig.async = sig.returnType.startsWith("Promise");
+            spec.signature = sig as t.CallableSignature;
+            console.debug(sig);
+         }
+      } else if (isListenersAssignment(text)) {
+         const regex = /(['"])(\w*)\1/g;
+         const matches = [...text.matchAll(regex)];
+         spec.listeners = matches.map((match) => match[2]);
+         // TODO: validate listeners format
+      }
+   }
+   node.forEachChild((child) => parseChannelExpressions(child, src, spec));
+}
+
+export function parseImportDeclarations(
+   node: ts.ImportDeclaration,
+   src: ts.SourceFile,
+   array: t.ImportSpec[],
+): void {
+   const customTypes = new Set<string>();
+   const moduleSpecifier = node.moduleSpecifier;
+   const clause = node.importClause;
+   const importSpec: t.ImportSpec = {
+      fromPath: moduleSpecifier.getText(src).slice(1, -1),
+      customTypes: [],
+      namespace: null,
+   };
+   if (clause?.namedBindings) {
+      if (ts.isNamespaceImport(clause.namedBindings)) {
+         importSpec.namespace = clause.namedBindings.name.getText(src);
+         array.push(importSpec);
+      } else if (ts.isNamedImports(clause.namedBindings)) {
+         clause.namedBindings.elements.forEach((element) => {
+            const name = element.name.getText(src);
+            if ((clause?.isTypeOnly || element.isTypeOnly) && !isBuiltinType(name)) {
+               customTypes.add(name);
+            }
+         });
+         importSpec.customTypes = Array.from(customTypes);
+         array.push(importSpec);
+      }
+   }
+}
+
+export function parseTypeDefinitions(
+   node: ts.InterfaceDeclaration | ts.TypeAliasDeclaration,
+   src: ts.SourceFile,
+   array: t.TypeSpec[],
+): void {
+   let isExported = false;
+   node.modifiers?.forEach((mod) => {
+      isExported = mod.kind === ts.SyntaxKind.ExportKeyword ? true : isExported;
    });
-   return funcSpecArray;
+   const kind = new Map([
+      ["InterfaceDeclaration", "interface"],
+      ["TypeAliasDeclaration", "type"],
+   ]).get(ts.SyntaxKind[node.kind]);
+   array.push({
+      name: node.name.getText(src),
+      kind: kind as t.TypeKind,
+      isExported,
+   });
 }
 
 export function parseSpecs(contents: string): t.ParsedSpecs {
-   const normalizedContents = utils.dedent(contents);
-   const regex = getParserRegex();
+   const channelSpecArray: t.ChannelSpec[] = [];
    const importSpecArray: t.ImportSpec[] = [];
    const typeSpecArray: t.TypeSpec[] = [];
-   const funcSignatures: string[] = [];
 
-   let match: RegExpExecArray | null = regex.exec(normalizedContents);
-   while (match !== null) {
-      const mg = match.groups;
-      const kind = (mg?.function ?? mg?.interface ?? mg?.type ?? mg?.import ?? "").trim();
-      if (kind === "function") {
-         funcSignatures.push(`${match[0].trimEnd()}}\n`);
-      } else if (["type", "interface"].includes(kind)) {
-         typeSpecArray.push({
-            kind: kind as t.TypeKind,
-            name: mg?.interfaceName ?? mg?.typeName ?? "",
-            isExported: [mg?.exportedInterface, mg?.exportedType].includes("export "),
-            definition: match[0],
-         });
-      } else if (kind === "import") {
-         importSpecArray.push({
-            kind: kind as t.ImportKind,
-            fromPath: mg?.importPath ?? "",
-            definition: match[0],
-            customTypes: Array.from(cctFromCode(match[0])),
-            namespace: mg?.namespace ?? null,
-         });
+   const src = ts.createSourceFile("temp.ts", contents, ts.ScriptTarget.Latest, true);
+
+   ts.forEachChild(src, (node: ts.Node) => {
+      if (ts.isExpressionStatement(node)) {
+         if (node.getText(src).startsWith("Channel")) {
+            const spec: Partial<t.ChannelSpec> = {};
+            parseChannelExpressions(node, src, spec);
+            // TODO: validate spec before pushing to array
+            channelSpecArray.push(spec as t.ChannelSpec);
+         }
+      } else if (ts.isImportDeclaration(node)) {
+         parseImportDeclarations(node, src, importSpecArray);
+      } else if (ts.isInterfaceDeclaration(node)) {
+         parseTypeDefinitions(node, src, typeSpecArray);
+      } else if (ts.isTypeAliasDeclaration(node)) {
+         parseTypeDefinitions(node, src, typeSpecArray);
       }
-      match = regex.exec(normalizedContents);
-   }
-   const funcSpecArray: t.FuncSpec[] = getFuncSpecs(funcSignatures.join(""), true);
-   return { funcSpecArray, typeSpecArray, importSpecArray };
+   });
+   return { channelSpecArray, typeSpecArray, importSpecArray };
 }
 
 export default {
-   getParserRegex,
    isBuiltinType,
    collectCustomTypes,
-   cctFromCode,
-   getFuncSpecs,
+   parseChannelExpressions,
+   parseImportDeclarations,
+   parseTypeDefinitions,
    parseSpecs,
 };
