2168a2d5791c8f4e93a3ec4d25ca243e99307e82
Mattias Aabmets <mattias.aabmets@gmail.com>
2024-11-22 09:50:46 +0200

    Added validateChannelSpec function

--- src/validators.ts
+++ src/validators.ts
@@ -11,7 +11,7 @@
 
 import path from "node:path";
 import * as t from "@types";
-import { assert, number, object, refine, string } from "superstruct";
+import { assert, array, boolean, number, object, refine, string } from "superstruct";
 import utils from "./utils.js";
 
 /**
@@ -29,14 +29,14 @@ import utils from "./utils.js";
  * @param config - Optional configuration object.
  * @returns The validated and resolved configuration.
  */
-export function validateResolveConfig(config: t.IPCOptionalConfig = {}): t.IPCAssuredConfig {
-   const mergedConfig: t.IPCAssuredConfig = {
+export function validateResolveConfig(config: t.IPCOptionalConfig = {}): t.IPCResolvedConfig {
+   const mergedConfig: t.IPCResolvedConfig = {
       ipcSpecPath: "src/ipc-spec.ts",
       rendererDir: "src/renderer",
       codeIndent: 3,
       ...config,
    };
-   const IPCAssuredConfigStruct = object({
+   const IPCResolvedConfigStruct = object({
       ipcSpecPath: refine(string(), "relative", (value) => {
          if (path.isAbsolute(value)) {
             return "ipcSpecPath must be relative to the project root";
@@ -62,11 +62,72 @@ export function validateResolveConfig(config: t.IPCOptionalConfig = {}): t.IPCAs
          return value >= 2 && value <= 4 ? true : errMsg;
       }),
    });
-   assert(mergedConfig, IPCAssuredConfigStruct);
+   assert(mergedConfig, IPCResolvedConfigStruct);
    return Object.assign(mergedConfig, {
       ipcSpecPath: utils.resolveUserProjectPath(mergedConfig.ipcSpecPath),
       rendererDir: utils.resolveUserProjectPath(mergedConfig.rendererDir),
    });
 }
 
-export default { validateResolveConfig };
+export function validateChannelSpec(spec: Partial<t.ChannelSpec>): t.ChannelSpec {
+   const ChannelSpecStruct = object({
+      name: refine(string(), "pascalcase", (value) => {
+         if (value.length < 3) {
+            return "Channel name must be at least 3 characters in length";
+         } else if (value.toLowerCase().startsWith("on")) {
+            return "Channel name must not begin with 'on'";
+         } else if (/^(?![A-Z])/.test(value)) {
+            return "Channel name must start with a capital letter";
+         } else {
+            return true;
+         }
+      }),
+      kind: refine(string(), "choice", (value) => {
+         const choices = ["Broadcast", "Unicast"];
+         if (choices.includes(value)) {
+            return true;
+         } else {
+            return `Channel kind must be one of '${choices}'`;
+         }
+      }),
+      direction: refine(string(), "choice", (value) => {
+         const choices = ["RendererToRenderer", "RendererToMain", "MainToRenderer"];
+         if (choices.includes(value)) {
+            return true;
+         } else {
+            return `Channel direction must be one of '${choices}'`;
+         }
+      }),
+      signature: object({
+         params: array(
+            object({
+               name: string(),
+               type: string(),
+               rest: boolean(),
+               optional: boolean(),
+            }),
+         ),
+         returnType: string(),
+         customTypes: array(string()),
+         async: boolean(),
+      }),
+      listeners: array(
+         refine(string(), "format", (value) => {
+            if (value.length < 5) {
+               const msg = "Channel listener names must be at least 5 characters in length";
+               return `'${value}'\n${msg}\n`;
+            } else if (!/^on[A-Z]\w+/.test(value)) {
+               const msg =
+                  "Channel listener names must begin with " +
+                  "lowercase 'on', followed by a capital letter";
+               return `'${value}'\n${msg}\n`;
+            }
+            return true;
+         }),
+      ),
+   });
+   assert(spec, ChannelSpecStruct);
+   return spec as t.ChannelSpec;
+}
+
+export default { validateResolveConfig, validateChannelSpec };
