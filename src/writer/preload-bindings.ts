b3975239b9a113c0af406a191149bd47579a5160
Mattias Aabmets <mattias.aabmets@gmail.com>
2024-12-01 00:15:39 +0200

    Implemented PreloadBindingsWriter functionality

--- src/writer/preload-bindings.ts
+++ src/writer/preload-bindings.ts
@@ -16,6 +16,34 @@ export class PreloadBindingsWriter extends BaseWriter {
       return this.resolvedConfig.preloadBindingsFilePath;
    }
    protected renderFileContents(): string {
-      return ""; // TODO
+      const out = [this.notice, 'import { contextBridge, ipcRenderer } from "electron/renderer";'];
+      const callables: string[] = [];
+
+      for (const parsedFileSpecs of this.pfsArray) {
+         for (const spec of parsedFileSpecs.specs.channelSpecArray) {
+            if (spec.direction === "RendererToMain") {
+               const method = spec.kind === "Broadcast" ? "send" : "invoke";
+               const ipcRenderer = `ipcRenderer.${method}('${spec.name}', ...args)`;
+               const callable = `send${spec.name}: (...args: any[]) => ${ipcRenderer}`;
+               callables.push(callable);
+            } else if (spec.direction === "MainToRenderer") {
+               const callback = "(_event: any, ...args: any[]) => callback(...args)";
+               const ipcRenderer = `ipcRenderer.on('${spec.name}', ${callback})`;
+               const callable = `on${spec.name}: (callback: Function) => ${ipcRenderer}`;
+               callables.push(callable);
+            } else if (spec.direction === "RendererToRenderer") {
+               // TODO: support
+            }
+         }
+      }
+      const sortedCallables = this.sortCallablesByPrefix(callables).join(`,\n${this.indents[0]}`);
+      const bindingsExpression = [
+         "\ncontextBridge.exposeInMainWorld('ipc', {",
+         `\n${this.indents[0]}${sortedCallables},`,
+         "\n});",
+      ].join("");
+
+      out.push(bindingsExpression);
+      return out.join("\n");
    }
 }
