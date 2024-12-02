c8d080e211b2b27be246d0de9cb0bf4bd7144a9d
Mattias Aabmets <mattias.aabmets@gmail.com>
2024-12-02 17:04:01 +0200

    PreloadBindingsWriter now supports RendererToRenderer message ports

--- src/writer/preload-bindings.ts
+++ src/writer/preload-bindings.ts
@@ -9,6 +9,7 @@
  *   SPDX-License-Identifier: Apache-2.0
  */
 
+import utils from "../utils.js";
 import { BaseWriter } from "./base-writer.js";
 
 export class PreloadBindingsWriter extends BaseWriter {
@@ -16,8 +17,9 @@ export class PreloadBindingsWriter extends BaseWriter {
       return this.resolvedConfig.preloadBindingsFilePath;
    }
    protected renderFileContents(): string {
-      const out = [this.notice, 'import { contextBridge, ipcRenderer } from "electron";'];
-      const callables: string[] = [];
+      const portInitializersArray: string[] = [];
+      const portNamesArray: string[] = [];
+      const callablesArray: string[] = [];
 
       for (const parsedFileSpecs of this.pfsArray) {
          for (const spec of parsedFileSpecs.specs.channelSpecArray) {
@@ -25,25 +27,66 @@ export class PreloadBindingsWriter extends BaseWriter {
                const method = spec.kind === "Broadcast" ? "send" : "invoke";
                const ipcRenderer = `ipcRenderer.${method}('${spec.name}', ...args)`;
                const callable = `send${spec.name}: (...args: any[]) => ${ipcRenderer}`;
-               callables.push(callable);
+               callablesArray.push(callable);
             } else if (spec.direction === "MainToRenderer") {
                const callback = "(_event: any, ...args: any[]) => callback(...args)";
                const ipcRenderer = `ipcRenderer.on('${spec.name}', ${callback})`;
                const callable = `on${spec.name}: (callback: Function) => ${ipcRenderer}`;
-               callables.push(callable);
+               callablesArray.push(callable);
             } else if (spec.direction === "RendererToRenderer") {
-               // TODO: support
+               portInitializersArray.push(this.getPortInitializer(spec.name).trim());
+               portNamesArray.push(spec.name);
             }
          }
       }
-      const sortedCallables = this.sortCallablesByPrefix(callables).join(`,\n${this.indents[0]}`);
+      const sortedCallablesArray = this.sortCallablesByPrefix(callablesArray);
+      const sortedCallables = sortedCallablesArray.join(`,\n${this.indents[0]}`);
       const bindingsExpression = [
          "\ncontextBridge.exposeInMainWorld('ipc', {",
          `\n${this.indents[0]}${sortedCallables},`,
-         "\n});\n",
-      ].join("");
+      ];
+      let out = [this.notice, 'import { contextBridge, ipcRenderer } from "electron";'];
+      if (portNamesArray.length > 0) {
+         out.push('import type { IpcRendererEvent } from "electron";');
+         out.push(this.getPortComponents());
+         out = out.concat(portInitializersArray);
+         const portNameProps = portNamesArray
+            .map((item) => `${item}: getPortObject('${item}')`)
+            .join(`,\n${this.indents[1]}`);
+         const ports = [
+            `\n${this.indents[0]}ports: {`,
+            `\n${this.indents[1]}${portNameProps},`,
+            `\n${this.indents[0]}},`,
+         ].join("");
+         bindingsExpression.push(ports);
+      }
+      bindingsExpression.push("\n});\n");
 
-      out.push(bindingsExpression);
+      out.push(bindingsExpression.join(""));
       return out.join("\n");
+   }
+
+   private getPortComponents() {
+      return utils.dedent(`
+         const ports: { [key: string]: MessagePort } = {};
+         
+         type PortObject = { sendMessage: Function, onMessage: Function };
+         function getPortObject(portName: string): PortObject {
+            return {
+               sendMessage: (...args: any[]) => ports[portName].postMessage(args),
+               onMessage: (callback: Function) => {
+                  ports[portName].onmessage = (event: MessageEvent) => callback(...event.data);
+               },
+            }
+         }
+      `);
+   }
+
+   private getPortInitializer(portName: string) {
+      return utils.dedent(`
+         ipcRenderer.on('${portName}', (event: IpcRendererEvent) => {
+            ports.${portName} = event.ports[0];
+         });
+      `);
    }
 }
