842425453a4056e3c508befbfc6534b74abcbd62
Mattias Aabmets <mattias.aabmets@gmail.com>
2024-12-09 17:22:11 +0200

    Implemented HMR for ipcAutomation plugin

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,7 @@
 import fsp from "node:fs/promises";
 import path from "node:path";
 import type * as t from "@types";
+import type { HmrContext } from "vite";
 import cfg from "./config.js";
 import logger from "./logger.js";
 import parser from "./parser.js";
@@ -22,64 +23,74 @@ export function ipcAutomation(): t.IpcAutomationPlugin {
       name: "vite-plugin-automate-electron-ipc",
       buildStart: async () => {
          const config = await cfg.getResolvedConfig();
-         const pfsArray: t.ParsedFileSpecs[] = [];
-
-         if (!config.ipcSchema.stats) {
-            logger.nonExistentSchemaPath(config.ipcSchema.path);
-            return;
-         } else if (config.ipcSchema.stats.isFile()) {
-            const contents = await fsp.readFile(config.ipcSchema.path);
-            const fileData: t.FileMeta = {
-               fullPath: config.ipcSchema.path,
-               relativePath: config?.ipcDataDir || "src/ipc",
-            };
-            const specs = parser.parseSpecs({
-               contents: contents.toString(),
-               ...fileData,
-            });
-            if (specs.channelSpecArray.length > 0) {
-               pfsArray.push({ specs: specs, ...fileData });
-            }
-         } else if (config.ipcSchema.stats.isDirectory()) {
-            const files = await fsp.readdir(config.ipcSchema.path, { recursive: true });
-            const rawFileContents: t.RawFileContents[] = [];
-            await Promise.all(
-               files.map(async (file) => {
-                  const fullPath = path.join(config.ipcSchema.path, file);
-                  const stat = await fsp.stat(fullPath);
-                  if (stat.isFile() && (await fsp.exists(fullPath))) {
-                     const contents = await fsp.readFile(fullPath);
-                     rawFileContents.push({
-                        fullPath,
-                        relativePath: file,
-                        contents: contents.toString(),
-                     });
-                  }
-               }),
-            );
-            for (const item of rawFileContents) {
-               const specs = parser.parseSpecs(item);
-               if (specs.channelSpecArray.length > 0) {
-                  pfsArray.push({
-                     fullPath: item.fullPath,
-                     relativePath: item.relativePath,
-                     specs: specs,
-                  });
-               }
-            }
-         }
-         await Promise.all([
-            new writer.MainBindingsWriter(config, pfsArray).write(),
-            new writer.PreloadBindingsWriter(config, pfsArray).write(),
-            new writer.RendererTypesWriter(config, pfsArray).write(),
-         ]);
-         if (pfsArray.length === 0) {
-            logger.noChannelExpressions(config.ipcSchema.path);
-         } else {
-            logger.reportSuccess(pfsArray);
+         await handleIpcAutomation(config);
+      },
+      handleHotUpdate: async (ctx: HmrContext) => {
+         const config = await cfg.getResolvedConfig();
+         if (ctx.file.startsWith(config.ipcSchema.path)) {
+            await handleIpcAutomation(config);
          }
       },
    };
+}
+
+async function handleIpcAutomation(config: t.IPCResolvedConfig) {
+   const pfsArray: t.ParsedFileSpecs[] = [];
+
+   if (!config.ipcSchema.stats) {
+      logger.nonExistentSchemaPath(config.ipcSchema.path);
+      return;
+   } else if (config.ipcSchema.stats.isFile()) {
+      const contents = await fsp.readFile(config.ipcSchema.path);
+      const fileData: t.FileMeta = {
+         fullPath: config.ipcSchema.path,
+         relativePath: config?.ipcDataDir || "src/ipc",
+      };
+      const specs = parser.parseSpecs({
+         contents: contents.toString(),
+         ...fileData,
+      });
+      if (specs.channelSpecArray.length > 0) {
+         pfsArray.push({ specs: specs, ...fileData });
+      }
+   } else if (config.ipcSchema.stats.isDirectory()) {
+      const files = await fsp.readdir(config.ipcSchema.path, { recursive: true });
+      const rawFileContents: t.RawFileContents[] = [];
+      await Promise.all(
+         files.map(async (file) => {
+            const fullPath = path.join(config.ipcSchema.path, file);
+            const stat = await fsp.stat(fullPath);
+            if (stat.isFile() && (await fsp.exists(fullPath))) {
+               const contents = await fsp.readFile(fullPath);
+               rawFileContents.push({
+                  fullPath,
+                  relativePath: file,
+                  contents: contents.toString(),
+               });
+            }
+         }),
+      );
+      for (const item of rawFileContents) {
+         const specs = parser.parseSpecs(item);
+         if (specs.channelSpecArray.length > 0) {
+            pfsArray.push({
+               fullPath: item.fullPath,
+               relativePath: item.relativePath,
+               specs: specs,
+            });
+         }
+      }
+   }
+   await Promise.all([
+      new writer.MainBindingsWriter(config, pfsArray).write(),
+      new writer.PreloadBindingsWriter(config, pfsArray).write(),
+      new writer.RendererTypesWriter(config, pfsArray).write(),
+   ]);
+   if (pfsArray.length === 0) {
+      logger.noChannelExpressions(config.ipcSchema.path);
+   } else {
+      logger.reportSuccess(pfsArray);
+   }
 }
 
 export function Channel(): t.Channels {
